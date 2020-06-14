17da1ce8f6bd77843d74c7af12bc72e51e04eada
Hector Bello Martinez <62455718+hbelloma@users.noreply.github.com>
2020-06-14 01:38:38 -0500

    Update GameControll.cs

--- GameControll.cs
+++ GameControll.cs
@@ -16,30 +16,27 @@ public class NewBehaviourScript : MonoBehaviour
 
 	// Start is called before the first frame update
 	void Start()
-    {
+	{
 		animator = my3DModel.GetComponent<Animator>();
 	}
-
 	// Update is called once per frame
- 
 	public void playAnim()
-    {
+	{
 		animator.Play("GirlshorT",-1,0f); // en parentesis el nombre de la animacion
 	}
 
 	public void pauseAnim()
-    {
+	{
 		animator.speed=0;
 		MyPauseButton.GetComponentInChildren<Text> ().text="RESUME";
 		Button btn=MyPauseButton.GetComponent<Button> ();
 		btn.onClick.AddListener(resumeAnim);
 	}
-	void resumeAnim)=
-    {
+	void resumeAnim()
+	{
 		MyPauseButton.GetComponentInChildren<Text> ().text="PAUSE";
 		animator.speed=1;
 		Button btn=MyPauseButton.GetComponent<Button> ();
 		btn.onClick.AddListener(pauseAnim);
 	}
-
 }
