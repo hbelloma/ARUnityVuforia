e46b70e5462ab4e82eff36ad3c1533d3612b652d
Hector Bello Martinez <62455718+hbelloma@users.noreply.github.com>
2020-06-14 01:25:35 -0500

    Update GameControll.cs

--- GameControll.cs
+++ GameControll.cs
@@ -1,18 +1,45 @@
+///*********************************************
+// Game Controler for buttons in AR for Vuforia
+//  Author Hector Bello, date 14/06/2020
+// **********************************************///
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Animator animator;
+	public Transform my3DModel;
+	public Transform MyPauseButton;
 
-    // Update is called once per frame
-    void Update()
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+		animator = my3DModel.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+ 
+	public void playAnim()
+    {
+		animator.Play("GirlshorT",-1,0f); // en parentesis el nombre de la animacion
+	}
+
+	public void pauseAnim()
+    {
+		animator.speed=0;
+		MyPauseButton.GetComponentInChildren<Text> ().text="RESUME";
+		Button btn=MyPauseButton.GetComponent<Button> ();
+		btn.onClick.AddListener(resumeAnim);
+	}
+	void resumeAnim)=
+    {
+		MyPauseButton.GetComponentInChildren<Text> ().text="PAUSE";
+		animator.speed=1;
+		Button btn=MyPauseButton.GetComponent<Button> ();
+		btn.onClick.AddListener(pauseAnim);
+	}
+
 }
